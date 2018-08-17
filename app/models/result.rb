8f04b147d6be8013d68579699e6acd4178c7eed4
bhargav <bhargav.r.raut@gmail.com>
2018-08-17 06:02:02 +0530

    ui is completed

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,11 +1,14 @@
 class Result
 	include Mongoid::Document
 	field :setup, type: String
+	field :setup_exit, type: String
 	field :triggered_at, type: Integer
 	## should have a field called, action, that is inferred from the user query, like 'buy' or 'sell'
 	## 0 -> buy
 	## 1 -> sell
 	field :trade_action, type: Integer
 	field :trade_action_name, type: String
+	field :trade_action_end, type: Integer
+	field :trade_action_end_name, type: String
 	embeds_many :impacts, :class_name => "Impact"
 end
