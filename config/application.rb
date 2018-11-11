8271dcbe840d038cdcf0e41aa920e8597607251b
greatmanta111 <bhargav.r.raut@gmail.com>
2018-11-11 15:59:04 +0530

    got rid of active record

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,16 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "active_model/railtie"
+
+require "action_controller/railtie"
+
+require "action_mailer/railtie"
+
+require "action_view/railtie"
+
+require "sprockets/railtie"
+
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,6 +34,6 @@ module Jafocomp
     config.autoload_paths += Dir["#{config.root}/lib/**/**/"]
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
