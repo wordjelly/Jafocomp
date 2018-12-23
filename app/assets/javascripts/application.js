a621fb9629869edc1f2ab9bfa3925b117ad4170c
bhargav raut <bhargav.r.raut@gmail.com>
2018-12-24 00:14:25 +0530

    added code to assemble contexts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -62,6 +62,90 @@ var render_search_result = function(search_result){
 	$("time.timeago").timeago();
 }
 
+var prepare_contexts = function(input){
+	
+	var existing_contexts = JSON.parse($("#top_result_contexts").attr("data-context"));
+	/***
+	convert the existing contexts array to something like this
+	{
+		"text" : length of text (4
+	}
+	***/
+	var existing_contexts_object = null;
+	if(!_.isNull(existing_contexts)){
+		var existing_contexts_object_values = _.map(existing_contexts,function(e){
+			return {
+				"words" : e.split(/\s/),
+				"length" : e.length/100,
+				"score" : 0,
+				"word_mapping" : {},
+				"original_key" : e
+			}
+		});
+		existing_contexts_object = _.object(existing_contexts,existing_contexts_object_values);
+	}
+
+	// remove multiple spaces
+	var context = input.replace(/\s{2}/g,'');
+	
+	// split on space.
+	context = context.split(/\s/);
+	
+	// take everything before the space
+	context = context.slice(0,-1);
+	
+	// get rid of the stopwords
+	context = _.reject(context, function(element){
+		_.has(stopwords,element);
+	});
+	
+	// sort alphabetically
+	context.sort();
+	
+	
+	if(_.isNull(existing_contexts_object)){
+		// in this case we just prepare the wordgrams ?
+		// or what
+	}
+	else{
+		_.each(context,function(word){
+			for(key in existing_contexts_object){
+				_.each(existing_contexts_object[key]["words"],function(eword){
+					if(eword.indexOf(word) != -1){
+						existing_contexts_object[key]["score"] += 1
+						if(!_.has(existing_contexts_object[key][word_mapping],word)){
+							existing_contexts_object[key][word_mapping][word] = eword;
+						}
+					}
+				});
+			}
+		});
+		// the score may be same, we use the length as a decimal.
+		for(key in existing_contexts_object){
+			existing_contexts_object[key]["score"] = existing_contexts_object[key]["score"] + (1/existing_contexts_object[key]["length"]);
+		}
+
+		// now sort by the best one.
+		// and what do you want of them.
+		// 
+		var sorted_values = _.sortBy(_.values(existing_contexts_object), function(o) { return o["score"]; })
+		// go over the context words now, 
+		// and get their mapped values from the first one of the sorted vlaues
+		// that is one context
+		// the other context is the first sorted value "original key"
+		// and those are the two contexts for the query.
+
+	}
+
+	// now prepare the wordgrams from the rewired words.
+
+}
+
+var prepare_query = function(input){
+	// the last word after the space becomes the prefix input.
+
+}
+
 // now add the impacts and from tomorrow start developing the springapp for a live online version
 
 var search = function(input){
@@ -86,6 +170,7 @@ var search = function(input){
 		}
 	});
 
+
 	console.log("query context:" + query_context);
 	console.log("last word of query:");
 	console.log(_.last(input.split(" ")));
@@ -96,8 +181,6 @@ var search = function(input){
 	  dataType: "json",
 	  data:{query: _.last(input.split(" ")), context: query_context}, 
 	  success: function(response){
-	  	
-
 
 	    $('#search_results').html("");
 	    
@@ -114,7 +197,6 @@ var search = function(input){
 	    	}
 	    	render_search_result(search_result);
 	    });
-	    
 	  }
 	});
 }
@@ -254,3 +336,186 @@ $(document).ready(function(){
 	$("#quote").text(quote);
 	$("#quote_author").text(quote_author); 
 });
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+/***
+List from : https://www.ranks.nl/stopwords
+Default English Stopwords List.
+****/
+var stopwords = {
+	"a":"1",
+	"about":"1",
+	"above":"1",
+	"after":"1",
+	"again":"1",
+	"against":"1",
+	"all":"1",
+	"am":"1",
+	"an":"1",
+	"and":"1",
+	"any":"1",
+	"are":"1",
+	"aren't":"1",
+	"as":"1",
+	"at":"1",
+	"be":"1",
+	"because":"1",
+	"been":"1",
+	"before":"1",
+	"being":"1",
+	"below":"1",
+	"between":"1",
+	"both":"1",
+	"but":"1",
+	"by":"1",
+	"can't":"1",
+	"cannot":"1",
+	"could":"1",
+	"couldn't":"1",
+	"did":"1",
+	"didn't":"1",
+	"do":"1",
+	"does":"1",
+	"doesn't":"1",
+	"doing":"1",
+	"don't":"1",
+	"down":"1",
+	"during":"1",
+	"each":"1",
+	"few":"1",
+	"for":"1",
+	"from":"1",
+	"further":"1",
+	"had":"1",
+	"hadn't":"1",
+	"has":"1",
+	"hasn't":"1",
+	"have":"1",
+	"haven't":"1",
+	"having":"1",
+	"he":"1",
+	"he'd":"1",
+	"he'll":"1",
+	"he's":"1",
+	"her":"1",
+	"here":"1",
+	"here's":"1",
+	"hers":"1",
+	"herself":"1",
+	"him":"1",
+	"himself":"1",
+	"his":"1",
+	"how":"1",
+	"how's":"1",
+	"i":"1",
+	"i'd":"1",
+	"i'll":"1",
+	"i'm":"1",
+	"i've":"1",
+	"if":"1",
+	"in":"1",
+	"into":"1",
+	"is":"1",
+	"isn't":"1",
+	"it":"1",
+	"it's":"1",
+	"its":"1",
+	"itself":"1",
+	"let's":"1",
+	"me":"1",
+	"more":"1",
+	"most":"1",
+	"mustn't":"1",
+	"my":"1",
+	"myself":"1",
+	"no":"1",
+	"nor":"1",
+	"not":"1",
+	"of":"1",
+	"off":"1",
+	"on":"1",
+	"once":"1",
+	"only":"1",
+	"or":"1",
+	"other":"1",
+	"ought":"1",
+	"our":"1",
+	"ours	ourselves":"1",
+	"out":"1",
+	"over":"1",
+	"own":"1",
+	"same":"1",
+	"shan't":"1",
+	"she":"1",
+	"she'd":"1",
+	"she'll":"1",
+	"she's":"1",
+	"should":"1",
+	"shouldn't":"1",
+	"so":"1",
+	"some":"1",
+	"such":"1",
+	"than":"1",
+	"that":"1",
+	"that's":"1",
+	"the":"1",
+	"their":"1",
+	"theirs":"1",
+	"them":"1",
+	"themselves":"1",
+	"then":"1",
+	"there":"1",
+	"there's":"1",
+	"these":"1",
+	"they":"1",
+	"they'd":"1",
+	"they'll":"1",
+	"they're":"1",
+	"they've":"1",
+	"this":"1",
+	"those":"1",
+	"through":"1",
+	"to":"1",
+	"too":"1",
+	"under":"1",
+	"until":"1",
+	"up":"1",
+	"very":"1",
+	"was":"1",
+	"wasn't":"1",
+	"we":"1",
+	"we'd":"1",
+	"we'll":"1",
+	"we're":"1",
+	"we've":"1",
+	"were":"1",
+	"weren't":"1",
+	"what":"1",
+	"what's":"1",
+	"when":"1",
+	"when's":"1",
+	"where":"1",
+	"where's":"1",
+	"which":"1",
+	"while":"1",
+	"who":"1",
+	"who's":"1",
+	"whom":"1",
+	"why":"1",
+	"why's":"1",
+	"with":"1",
+	"won't":"1",
+	"would":"1",
+	"wouldn't":"1",
+	"you":"1",
+	"you'd":"1",
+	"you'll":"1",
+	"you're":"1",
+	"you've":"1",
+	"your":"1",
+	"yours":"1",
+	"yourself":"1",
+	"yourselves":"1"
+}
