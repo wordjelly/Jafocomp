25b0e649ed6c39572fc2d452a101116468fae57b
bhargav raut <bhargav.r.raut@gmail.com>
2019-07-01 00:01:44 +0530

    with sectors

--- config/initializers/sectors.rb
+++ config/initializers/sectors.rb
@@ -1,3 +1,5 @@
 sector_hashes = Sector::load_sectors
+puts "the sector hashes are:"
+puts sector_hashes.to_s
 $sectors = sector_hashes[:sector_counter_to_name]
 $sectors_name_to_counter = sector_hashes[:sector_name_to_counter]
