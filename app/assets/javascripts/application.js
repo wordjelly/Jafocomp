9a88714083916b0e7bc4e2242e476f4b6d042ac7
Bhargav Raut <bhargav.r.raut@gmail.com>
2019-07-30 17:52:43 +0530

    changed buy to what happens to

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -202,10 +202,10 @@ var build_setup = function(search_result){
 	//console.log("complex string becomes:");
 	//console.log(complex_string);
 
-	search_result.setup = search_result.setup + " " + complex_string;
-	var parts = search_result.setup.split(/\'/)
+	search_result.setup = search_result.setup + " " + complex_string;	
+	var parts = search_result.setup.split(/\'/);
 	// find the apostrophe
-	search_result.setup = parts[0] + '...' + '<span style="display:none"'> + parts[1] +  '</span>';
+	search_result.setup = parts[0] + '...' + '<span style="display:none">' + parts[1] +  '</span>';
 	
 }
 
